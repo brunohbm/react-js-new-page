1637533bce4091ecb850c39c9c02024ec520b649
Bruno Henrique <brunohbm23@gmail.com>
2020-07-09 17:47:48 -0300

    Main page initial animation, done.

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -4,10 +4,12 @@ import hand from '../../assets/images/hand.png';
 
 const MainPage = () => (
 	<div className="main-page">
-		<h1 className="first-part">HOW CAN I</h1>
-		<h1 className="second-part">HOW CAN I</h1>
-		<h1 className="first-part">HELP YOU ?</h1>
-		<h1 className="second-part">HELP YOU ?</h1>
+		<h1 className="first-text-part">HOW CAN I</h1>
+		<h1 className="second-text-part">HOW CAN I</h1>
+		<h1 className="third-text-part">HELP YOU ?</h1>
+		<h1 className="fouth-text-part">HELP YOU ?</h1>
+		<h1 className="fifth-text-part">I</h1>
+		<h1 className="fifth-text-part">I</h1>
 		<img className="down-hand" src={hand} alt="No image" />
 		<img className="up-hand" src={hand} alt="No image" />
 	</div>
