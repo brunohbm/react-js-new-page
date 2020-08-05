1950d82af3547f2727015c0c76bc013d4449db31
Bruno Henrique <brunohbm23@gmail.com>
2020-08-04 22:30:14 -0300

    Adding carrer texts.

--- src/pages/PortfolioPage.js
+++ src/pages/PortfolioPage.js
@@ -24,22 +24,22 @@ const PortfolioPage = () => (
 		<Col
             title="College"
             period="2015 - 2017"
-            description="Test"
+            description="I did a course at the college called Technology in Analysis and System Development, where I learned many technologies and principles."
         />
         <Col
             title="Freelancer"
             period="2017 - 2018"
-            description="Test"
+            description="When I finished college I had the opportunity to work as a freelancer. Where I did websites and learned some new skills as a programmer."
         />
         <Col
             title="Appmoove"
             period="2018 - 2020"
-            description="Test"
+            description="I was hired as a web developer and during this period I learned new technologies, such as react js, ANTD, test with jest. And I learned how to work on a group, set priorities and many other skills."
         />
         <Col
             title="Smarppy"
             period={`2020 - ${year}`}
-            description="Test"
+            description="I'm currently a web developer and I work with many technologies, such as react js and ANTD to develop great systems with an amazing team <3"
         />
         <div className="carrer-text">
             Career
